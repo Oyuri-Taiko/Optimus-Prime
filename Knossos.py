aafcb4af4a080a206b7e38ca9a0099ad0571b3b1
Krycha <oyuri.taiko@gmail.com>
2021-07-26 00:04:53 +0200

    Fck Python arrays

--- Knossos.py
+++ Knossos.py
@@ -1,15 +1,17 @@
-import array
+import numpy as np
 
 labRow = int(input("Rzędy: "))
 labColumn = int(input("Kolumny: "))
-lab = [[0]*labColumn]*labRow
+lab = np.ones((labRow, labColumn), dtype=int)
 print("\n")
 
+lab[1:-1,1:-1] = 0
+  
 for x in lab:
   for y in x:
-    if y == 0:
-      print("X", end = " ")
+    if y == 1:
+      print("X", end = "")
     else:
-      print(" ", end = " ")
+      print(" ", end ="")
   print()
 input()
