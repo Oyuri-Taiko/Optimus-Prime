70cf8196fccc43edb1ede42122df139fe605f56e
Oyuri-Taiko <oyuri.taiko@gmail.com>
2021-08-01 17:50:17 +0200

    Działa

--- Knossos.py
+++ Knossos.py
@@ -6,7 +6,27 @@ import time
 
 
 ## Functions
-def printlab():
+
+# Creating starting and ending point
+
+def setEntrance():
+	for i in range(0, labWidth):
+		if (lab[1][i] == 'c'):
+			lab[0][i] = 'c'
+			start.append(i)
+		
+			break
+
+def setExit():
+	for i in range(labWidth-1, 0, -1):
+		if (lab[labHeight-2][i] == 'c'):
+			lab[labHeight-1][i] = 'c'
+			end.append(i)
+			break
+
+## Prints
+
+def printLab():
 	for x in lab:
 		for y in x:
 			if y == 'c':
@@ -14,7 +34,52 @@ def printlab():
 			else:
 				print("X", end = " ")
 		print()	
+
+#Zwykły nie działa normalnie
+
+def printSolvedLab(): 
+	for i in range(len(lab)):
+		for j in range(len(lab[i])):
+			print(str(lab[i][j]).ljust(2),end=' ')
+		print()
+
+# Printing the shortest path
+def ariadna():
+	for x in lab:
+		for y in x:
+			if isinstance(y, int):
+				print(" ", end = " ")
+			elif y == 'w':
+				print("X", end = " ")
+			else:
+				print("|", end = " ")
+		print()	
+##
+
 		
+# Finding the way to the end
+
+def tezeusz(k):
+	for i in range(len(lab)):
+		for j in range(len(lab[i])):
+			if lab[i][j] == k:
+				if lab[i-1][j] == 0:
+					lab[i-1][j] = k + 1
+				if lab[i][j-1] == 0:
+					lab[i][j-1] = k + 1
+				if lab[i+1][j] == 0:
+					lab[i+1][j] = k + 1
+				if lab[i][j+1] == 0 :
+					lab[i][j+1] = k + 1
+
+# Swapping data 
+def rubble(x, y):
+	for i in range(0, labHeight):
+		for j in range(0, labWidth):
+			if (lab[i][j] == x):
+				lab[i][j] = y
+		
+
 
 # Find number of surrounding cells
 def surroundingCells(rand_wall):
@@ -36,9 +101,12 @@ def surroundingCells(rand_wall):
 wall = 'w'
 cell = 'c'
 unvisited = 'u'
+k = 0
 labHeight = int(input("Rzędy: "))
 labWidth = int(input("Kolumny: "))
 lab = []
+start = [0]
+end = [labHeight-1]
 print("\n")
 
 
@@ -234,24 +302,51 @@ while (walls):
 
 
 # Mark the remaining unvisited cells as walls
-for i in range(0, labHeight):
-	for j in range(0, labWidth):
-		if (lab[i][j] == 'u'):
-			lab[i][j] = 'w'
+rubble('u', 'w')
+
 
 # Set entrance and exit
-for i in range(0, labWidth):
-	if (lab[1][i] == 'c'):
-		lab[0][i] = 'c'
-		break
-
-for i in range(labWidth-1, 0, -1):
-	if (lab[labHeight-2][i] == 'c'):
-		lab[labHeight-1][i] = 'c'
-		break
+setEntrance()
+setExit()
 
 # Print final lab
-printlab()
+printLab()
 
+input("\nCzas przez niego przejść \n")
+print("Punkt początkowy to:", start)
+print("Punkt końcowy to:", end, "\n")
+
+rubble('c', 0)
+lab[start[0]][start[1]] = 1
+
+while lab[end[0]][end[1]] == 0:
+    k += 1
+    tezeusz(k)
+
+
+i, j = end
+k = lab[i][j]
+while k > 1:
+	if lab[i - 1][j] == k-1:
+		i, j = i-1, j
+		lab[i][j] = 'p'
+		k-=1
+	elif lab[i][j - 1] == k-1:
+		i, j = i, j-1
+		lab[i][j] = 'p'
+		k-=1
+	elif lab[i + 1][j] == k-1:
+		i, j = i+1, j
+		lab[i][j] = 'p'
+		k-=1
+	elif lab[i][j + 1] == k-1:
+		i, j = i, j+1
+		lab[i][j] = 'p'
+		k -= 1	
+
+lab[end[0]][end[1]] = 'p'
+print("\n")
+rubble('p', '|')
+ariadna()
 
 input()
