b5a645d1967a5bcfaea15c17c0c1b257a68bd4b1
Oyuri-Taiko <oyuri.taiko@gmail.com>
2021-08-07 19:33:09 +0200

    Dobbie is a free elf

--- Knossos.py
+++ Knossos.py
@@ -42,56 +42,59 @@ def setStartingWalls():
 
 # Check which random wall was selected
 def checkWalls():
-	if (rand_wall[1] != 0): # left
-		if (lab[rand_wall[0]][rand_wall[1]-1] == 'u' and lab[rand_wall[0]][rand_wall[1]+1] == 'c'):
+	look = Point (rand_wall.x, rand_wall.y)
+	if (rand_wall.y != 0): # left
+		if look.left(lab) == 'u' and look.right(lab) == 'c':
 			return 'Left'
 
-	if (rand_wall[0] != 0): # upper
-		if (lab[rand_wall[0]-1][rand_wall[1]] == 'u' and lab[rand_wall[0]+1][rand_wall[1]] == 'c'):
+	if (rand_wall.x != 0): # upper
+		if look.up(lab) == 'u' and look.down(lab) == 'c':
 			return 'Top'
 		
-	if (rand_wall[0] != labHeight-1): # bottom
-		if (lab[rand_wall[0]+1][rand_wall[1]] == 'u' and lab[rand_wall[0]-1][rand_wall[1]] == 'c'):
+	if (rand_wall.x != labHeight-1): # bottom
+		if look.down(lab) == 'u' and look.up(lab) == 'c':
 			return 'Bottom'
 
-	if (rand_wall[1] != labWidth-1): # right
-		if (lab[rand_wall[0]][rand_wall[1]+1] == 'u' and lab[rand_wall[0]][rand_wall[1]-1] == 'c'):
+	if (rand_wall.y != labWidth-1): # right
+		if look.right(lab) == 'u' and look.left(lab) == 'c':
 			return 'Right'
 		
 	return 'Invalid'
 
 def buildWall(wall):
+	look = Point (rand_wall.x, rand_wall.y)
 	if wall == 'Right':
-		if (lab[rand_wall[0]][rand_wall[1]+1] != 'c'):
-			lab[rand_wall[0]][rand_wall[1]+1] = 'w'
-		if ([rand_wall[0], rand_wall[1]+1] not in walls):
-			walls.append([rand_wall[0], rand_wall[1]+1])
+		if look.right(lab) != 'c':
+			lab[rand_wall.x][rand_wall.y + 1] = 'w'
+		if ([rand_wall.x, rand_wall.y + 1] not in walls):
+			walls.append([rand_wall.x, rand_wall.y + 1])
 	elif wall == 'Down':
-		if (lab[rand_wall[0]+1][rand_wall[1]] != 'c'):
-			lab[rand_wall[0]+1][rand_wall[1]] = 'w'
-		if ([rand_wall[0]+1, rand_wall[1]] not in walls):
-			walls.append([rand_wall[0]+1, rand_wall[1]])
+		if look.down(lab) != 'c':
+			lab[rand_wall.x + 1][rand_wall.y] = 'w'
+		if ([rand_wall.x + 1, rand_wall.y] not in walls):
+			walls.append([rand_wall.x + 1, rand_wall.y])
 	elif wall == 'Up':	
-		if (lab[rand_wall[0]-1][rand_wall[1]] != 'c'):
-			lab[rand_wall[0]-1][rand_wall[1]] = 'w'
-		if ([rand_wall[0]-1, rand_wall[1]] not in walls):
-			walls.append([rand_wall[0]-1, rand_wall[1]])
+		if look.up(lab) != 'c':
+			lab[rand_wall.x - 1][rand_wall.y] = 'w'
+		if ([rand_wall.x - 1, rand_wall.y] not in walls):
+			walls.append([rand_wall.x - 1, rand_wall.y])
 	elif wall == 'Left':
-		if (lab[rand_wall[0]][rand_wall[1]-1] != 'c'):
-			lab[rand_wall[0]][rand_wall[1]-1] = 'w'
-		if ([rand_wall[0], rand_wall[1]-1] not in walls):
-			walls.append([rand_wall[0], rand_wall[1]-1])
+		if look.left(lab) != 'c':
+			lab[rand_wall.x][rand_wall.y - 1] = 'w'
+		if ([rand_wall.x, rand_wall.y - 1] not in walls):
+			walls.append([rand_wall.x, rand_wall.y - 1])
 
 # Find number of surrounding cells
 def surroundingCells(rand_wall):
+	look = Point (rand_wall.x, rand_wall.y)
 	s_cells = 0
-	if (lab[rand_wall[0]-1][rand_wall[1]] == 'c'):
+	if look.up(lab) == 'c':
 		s_cells += 1
-	if (lab[rand_wall[0]+1][rand_wall[1]] == 'c'):
+	if look.down(lab) == 'c':
 		s_cells += 1
-	if (lab[rand_wall[0]][rand_wall[1]-1] == 'c'):
+	if look.left(lab) == 'c':
 		s_cells +=1
-	if (lab[rand_wall[0]][rand_wall[1]+1] == 'c'):
+	if look.right(lab) == 'c':
 		s_cells += 1
 
 	return s_cells
@@ -99,32 +102,33 @@ def surroundingCells(rand_wall):
 
 def fillLab():
 	global rand_wall
-	rand_wall = walls[int(random.random()*len(walls))-1]  # Pick a random wall
+	test = (walls[int(random.random()*len(walls))-1])
+	rand_wall = Point(test[0], test[1])  # Pick a random wall
 	selectedWall = checkWalls() # Check which wall was chosen at random
 	
 	if selectedWall != 'Invalid':
 		s_cells = surroundingCells(rand_wall)	
 		if (s_cells < 2):
 			# Denote the new path	
-			lab[rand_wall[0]][rand_wall[1]] = 'c'
+			lab[rand_wall.x][rand_wall.y] = 'c'
 
-			if (rand_wall[1] != labWidth-1) and selectedWall != 'Left':	# Mark right cell as wall
+			if (rand_wall.y != labWidth-1) and selectedWall != 'Left':	# Mark right cell as wall
 				buildWall('Right')
 
-			if (rand_wall[0] != labHeight-1) and selectedWall != 'Top':	# Mark bottom cell as wall
+			if (rand_wall.x != labHeight-1) and selectedWall != 'Top':	# Mark bottom cell as wall
 				buildWall('Down')
 
-			if (rand_wall[0] != 0) and selectedWall != 'Bottom':	# Mark upper cell as wall
+			if (rand_wall.x != 0) and selectedWall != 'Bottom':	# Mark upper cell as wall
 				buildWall('Up')
 
-			if (rand_wall[1] != 0) and selectedWall != 'Right':	# Mark left cell as wall
+			if (rand_wall.y != 0) and selectedWall != 'Right':	# Mark left cell as wall
 				buildWall('Left')
 
 			for wall in walls:	# Delete random wall from the wall list
-				if (wall[0] == rand_wall[0] and wall[1] == rand_wall[1]):
+				if (wall[0] == rand_wall.x and wall[1] == rand_wall.y):
 					walls.remove(wall)
 	for wall in walls:
-		if (wall[0] == rand_wall[0] and wall[1] == rand_wall[1]):
+		if (wall[0] == rand_wall.x and wall[1] == rand_wall.y):
 			walls.remove(wall)
 
 
@@ -201,14 +205,15 @@ def tracePath(step):
 	i, j = end
 	step = lab[i][j]
 	while step > 0:
+		look = Point (i, j)
 		lab[i][j] = 'p'
-		if lab[i - 1][j] == step-1:
+		if look.up(lab) == step-1:
 			i, j = i-1, j
-		elif lab[i][j - 1] == step-1:
+		elif look.left(lab) == step-1:
 			i, j = i, j-1
-		elif lab[i + 1][j] == step-1:
+		elif look.down(lab) == step-1:
 			i, j = i+1, j
-		elif lab[i][j + 1] == step-1:
+		elif look.right(lab) == step-1:
 			i, j = i, j+1
 		step -= 1
 		animatedPrint()
@@ -248,6 +253,18 @@ start = []
 end = []
 print("\n")
 
+class Point:
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+	def up(self, array):
+		return array[self.x - 1][self.y]
+	def down(self, array):
+		return array[self.x + 1][self.y]
+	def left(self, array):
+		return array[self.x][self.y - 1]
+	def right(self, array):
+		return array[self.x][self.y + 1]
 
 labBuilder()
 
